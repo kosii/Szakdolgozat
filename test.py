eb825903f8ccc2b9b10956a351eecd6f1a5e6362
Balazs KOSSOVICS <balazs.kossovics@e-loue.com>
2011-10-26 17:42:27 +0200

    finding section names by virtual and physical address

--- test.py
+++ test.py
@@ -3,8 +3,23 @@ import sys
 import re
 import mmap
 import contextlib
+from pprint import pprint
 
 import pefile
+
+def get_sectionname_by_physical_address(pe, phys_addr):
+
+    for section in pe.sections:
+        if section.PointerToRawData <= phys_addr and phys_addr < section.PointerToRawData + section.SizeOfRawData:
+            return section.Name
+    return None
+
+def get_sectionname_by_virtual_address(pe, virtual_address):
+
+    for section in pe.sections:
+        if pe.OPTIONAL_HEADER.ImageBase + section.VirtualAddress <= virtual_address and virtual_address < pe.OPTIONAL_HEADER.ImageBase + section.VirtualAddress + section.SizeOfRawData:
+            return section.Name
+    return None 
 
 def little_endian_string_to_number(address):
     return reduce(lambda x, y: x*256 + y, reversed(map(ord, address)))
@@ -34,8 +49,18 @@ compiled_regexp = re.compile(regexp_pattern, flags=re.DOTALL)
 
 fd = os.open('ftp.exe', os.O_RDWR)
 with contextlib.closing(mmap.mmap(fd, length=0)) as mmapped_file:
-    print len(mmapped_file)
-    print len(compiled_regexp.findall(mmapped_file))
+    pe = pefile.PE(data=mmapped_file)
+    pprint(pe)
+    print hex(pe.OPTIONAL_HEADER.AddressOfEntryPoint)
+    print hex(pe.OPTIONAL_HEADER.ImageBase)
+    print pe.sections['.text']
+    for section in pe.sections:
+        pprint(section)
+        print section.Name, section.VirtualAddress, hex(section.VirtualAddress), type(section.VirtualAddress), section.SizeOfRawData
+    #print len(mmapped_file)
+    #print len(compiled_regexp.findall(mmapped_file))
     #print "asda:", len(re.findall(bytes([0x8B, 0x41, 0x04, 0x8b, 0x40, 0x18, 0x85, 0xc0, 0x75,0x05, 0xb8, ord('('), ord('.'), ord('.'), ord('.'), ord('.'), ord(')'), 0xc3]), mmapped_file))
     for i, matchObject in enumerate(compiled_regexp.finditer(mmapped_file)):
-        print i, hex(matchObject.start()), hex(little_endian_string_to_number(matchObject.group(1)))
+        pass
+        #print [hex(ord(b)) for b in matchObject.group(1)]
+        print i, get_sectionname_by_physical_address(pe, matchObject.start()), hex(matchObject.start()), get_sectionname_by_virtual_address(pe, little_endian_string_to_number(matchObject.group(1))), hex(little_endian_string_to_number(matchObject.group(1)))
