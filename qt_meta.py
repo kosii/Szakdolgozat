47f74f092567e08c68a337fd674c03ea93386cf2
Balazs KOSSOVICS <balazs.kossovics@e-loue.com>
2011-12-13 17:54:08 +0100

    we are gonna use __metaclass__ member for all class unless we find a better solution

--- qt_meta.py
+++ qt_meta.py
@@ -4,16 +4,14 @@ import collections
 #class QMetaClassInfo(namedtuple('QMetaClassInfo', 'name, key')):
 #    pass
 
-def struct_compiler(name, bases, dict):
-    print dict
-    print bases
+def descriptor_metaclass(name, bases, dict):
     if 'struct' in dict:
         dict['struct'] = struct.Struct(dict['struct'])
     return type(name, bases, dict)
 
 class MetaClassParser(object):
 
-    __metaclass__ = struct_compiler
+    __metaclass__ = descriptor_metaclass
 
     def __new__(cls, memory_image):
         if not hasattr(cls, 'struct') or not hasattr(cls, 'fields'):
@@ -24,38 +22,19 @@ class MetaClassParser(object):
     def namedtuple_factory(cls):
         return collections.namedtuple(cls.__name__, cls.fields)
 
-print MetaClassParser.__class__
-print 1, type(MetaClassParser)
+class QMetaClassInfoDescriptor(MetaClassParser):
 
-class QMetaClassInfo(MetaClassParser):
-    struct = "ii"
+    __metaclass__  = descriptor_metaclass
+
+    struct = 'ii'
     fields = 'name, key'
 
-print QMetaClassInfo.struct
-print QMetaClassInfo('\01\00\00\00\00\01\00\00')
+class QMetaMethodDesciptor = struct_compiler(
+    'QMetaMethodDesciptor', (MetaClassParser, ), 
+    {'struct': 'ii', 'fields': 'signature, parameters, type, tag, flags'})
 
-class QMetaMethod(object):
+QMetaEnum = struct_c
 
-    def methodIndex(self):
-        pass
-    
-    def methodType(self):
-        pass
-    
-    def parameterNames(self):
-        pass
-    
-    def parameterTypes(self):
-        pass
-    
-    def signature(self):
-        pass
-    
-    def tag(self):
-        pass
-    
-    def typeName(self):
-        pass
 class QMetaEnum(object):
     def isFlag(self):
         pass
